15b9d24e4cad5b473f7411db3955841b69280da7
Ftognola99 <98433644+Ftognola99@users.noreply.github.com>
2022-06-22 23:42:31 -0300

    js

--- src/App.js
+++ src/App.js
@@ -2,23 +2,12 @@ import './App.css';
 import Body from './components/body';
 import Navbar from './components/navbar';
 import Contador from './components/contador';
-import Card from './components/card';
-import Contenedor from './components/contenedor1';
-
-
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
       <Body/>
-      <Card/>
-      <Contenedor/>
-      
-      
-      
-      
-       
     </div>
   );
 }
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -6,18 +6,16 @@ function NavBar() {
   return (
 <Navbar bg="light" expand="lg">
   <Container>
-    <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+    <Navbar.Brand href="#home">Acer crom</Navbar.Brand>
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
       <Nav className="me-auto">
-        <Nav.Link href="#home">Home</Nav.Link>
-        <Nav.Link href="#link">Link</Nav.Link>
+        <Nav.Link href="#home">Inicio</Nav.Link>
+        <Nav.Link href="#link">Productos</Nav.Link>
         <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-          <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-          <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-          <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-          <NavDropdown.Divider />
-          <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.1">Sobre nosotros</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.2">Nuestra historia</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.3">Nuestra fabrica</NavDropdown.Item>
         </NavDropdown>
       </Nav>
     </Navbar.Collapse>
