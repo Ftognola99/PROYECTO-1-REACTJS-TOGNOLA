09cb726738cc515bd3da692f99c54e18cfe2946a
Ftognola99 <98433644+Ftognola99@users.noreply.github.com>
2022-06-29 18:01:22 -0300

    Map

---
+++ src/components/data/productos.js
@@ -0,0 +1,35 @@
+export const products = [
+    {
+        id : 1,
+        title: "teclado Red Dragon Kumara",
+        stock: 3,
+        price: 6000,
+        image: ""
+
+    },
+    {
+        id : 2,
+        title: "teclado Razer Chroma",
+        stock: 3,
+        price: 6500,
+        image: ""
+
+    },
+    {
+        id : 3,
+        title: "teclado giganet x45",
+        stock: 3,
+        price: 6000,
+        image: ""
+
+    },
+    {
+        id : 4,
+        title: "teclado Red Dragon Fruitpunch",
+        stock: 3,
+        price: 6000,
+        image: ""
+
+    },
+
+]
