beb55571384ba624b51b01508dd12611aa9a6587
Ftognola99 <98433644+Ftognola99@users.noreply.github.com>
2022-07-04 17:45:00 -0300

    contador js

--- src/components/contador.js
+++ src/components/contador.js
@@ -1,38 +1,38 @@
-import { useState } from "react";
+import React ,{ useState } from "react"
 
-function Contador(){
-const [suma, setSuma] = useState(0)
-const [otraSuma, setOtrasuma] = useState(0)
-
-useEffect(()=>{
-    console.log('efecto');
-})
-
-userEffect(()=>{
-    console.log('efecto en mounting');
-})
-
-useEffect(()=>{
-    console.log('efecto en cambio de suma');
-})
-
-const onClickHander=()=>{
-    console.log('click onClickHander')
-    setSuma(suma + 1)
-}
-
-const otraSumaHandler = () =>{
-    console.log('click onOtraSumaHandler')
-    setOtrasuma(Otrasuma + 1 )
-}
-return (
-    <div>
-        <button onClick={onClickHander} classname="btn">Click Aqui!!</button> 
-        <button onClick={otraSumaHandler} classname="btn">Otra suma</button> 
-    </div>
-)
+const ItemCounter = ({stock,initial,onAdd}) => {
+    const [contador,setContador] = useState(initial)
     
 }
+
+const fnSumar =() => {
+    if(contador < stock){
+        setContador(contador + 1)
+    }
+}
+
+const fnRestar = () => {
+    if(contador > stock){
+        setContador(contador - 1)
+    }
+}
+
+return (
+    <>
+    <div>
+        <button onClick={fnrestar}> click aqui para restar!!  </button>
+        <span>{contador}</span>
+        <button onClick={fnSumar}>Click aqui para sumar!!</button>
+    </div>
+    <button className="btn btn-primary" onClick={onAdd}>Agregar al carrito!!</button>
+    
+    </>
+    
+)
+
+
+
+
 
 
 export default Contador;
